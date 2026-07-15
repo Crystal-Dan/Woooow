4a88af9bd5c309ea034b0bca227a6cca7df28b97
Crystal-Dan <danya.illarionov.2018@mail.ru>
2026-07-16 02:04:40 +1000

    Update app.py

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def places():
     return jsonify(PLACES)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
