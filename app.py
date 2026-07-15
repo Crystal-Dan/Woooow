d6040bbb27363b587cfc1d83dd75031403509745
Crystal-Dan <danya.illarionov.2018@mail.ru>
2026-07-16 02:15:24 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+
 
 PLACES = [
     {
@@ -95,7 +98,6 @@ def index():
 def places():
     return jsonify(PLACES)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    print(f"Запуск на порту {port}", file=sys.stderr)
-    app.run(host='0.0.0.0', port=port, debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
